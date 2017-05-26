f4342f39ef77764563437bb8f4cad53e3df5b589
iyke <nderiony95@gmail.com>
2017-05-26 13:03:13 +0300

    add new js lines

--- js/git-interface.js
+++ js/git-interface.js
@@ -3,8 +3,8 @@ var Repos = require('./../js/git.js').reposModule;
 $(document).ready(function() {
   var getRepoInfo = new getRepos();
   $('#repoInfo').click(function() {
-    var city = $('#username').val();
+    var hub = $('#username').val();
     $('#username').val("");
-    getRepoInfo.getRepos(city);
+    getRepoInfo.getRepos(hub);
   });
 });
--- js/git.js
+++ js/git.js
@@ -1,6 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
+function git(){
+
+}
+git.prototype.getRepos = function(){
   $.get('https://api.github.com/users/' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
